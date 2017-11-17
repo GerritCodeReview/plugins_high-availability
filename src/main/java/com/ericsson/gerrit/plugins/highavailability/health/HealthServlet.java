ea7482771384193719fc27a1a185dd9047c057b4
David Pursehouse <dpursehouse@collab.net>
2017-11-17 16:18:18 +0900

    Initial implementation of health servlet
    The health servlet allows the plugin to be used to return responses
    to HAProxy's health check function [1].  For example if the http(s)
    backend is configured with something like:
    
      option httpchk GET /plugins/high-availability/health
      http-check expect status 200
    
    then GET calls to the /health URL will return HTTP status 200, thus
    indicating that the server is healthy (the default response).
    
    Sending a DELETE request to the same /health URL will set the status
    to 'unhealthy', and then any subsequent GET requests will return HTTP
    500.
    
    Sending a POST request sets the status back to 'healthy'.
    
    TODO: documentation and tests.
    
    [1] https://www.haproxy.com/documentation/aloha/7-0/haproxy/healthchecks/
    
    Change-Id: Ife2e6176347c5a84f328b9652a2613080572c994

---
+++ src/main/java/com/ericsson/gerrit/plugins/highavailability/health/HealthServlet.java
@@ -0,0 +1,61 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.ericsson.gerrit.plugins.highavailability.health;
+
+import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+
+import com.google.inject.Singleton;
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class HealthServlet extends HttpServlet {
+  private static final Logger LOG = LoggerFactory.getLogger(HealthServlet.class);
+  private static final long serialVersionUID = -1L;
+
+  private boolean healthy;
+
+  HealthServlet() {
+    this.healthy = true;
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse rsp) {
+    this.healthy = true;
+  }
+
+  @Override
+  protected void doDelete(HttpServletRequest req, HttpServletResponse rsp) {
+    this.healthy = false;
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse rsp) {
+    if (healthy) {
+      rsp.setStatus(SC_OK);
+    } else {
+      try {
+        rsp.sendError(SC_INTERNAL_SERVER_ERROR);
+      } catch (IOException e) {
+        LOG.error("Failed to send error response", e);
+      }
+    }
+  }
+}
