5507b8a467e763f0353ee5c246982b85d83ade16
David Pursehouse <dpursehouse@collab.net>
2017-11-17 16:18:18 +0900

    Initial implementation of health servlet
    The health servlet allows the plugin to be used to return responses
    to HAProxy's health check function [1].  For example if the http(s)
    backend is configured with something like:
    
      option httpchk GET /plugins/high-availability/health
    
    then GET calls to the /health URL will return HTTP status 204, thus
    indicating that the server is healthy (the default response).
    
    Sending a DELETE request to the same /health URL will set the status
    to 'unhealthy', and then any subsequent GET requests will return HTTP
    503.
    
    Sending a POST request sets the status back to 'healthy'.
    
    TODO: documentation and fix failing IT tests.
    
    [1] https://www.haproxy.com/documentation/aloha/7-0/haproxy/healthchecks/
    
    Change-Id: Ife2e6176347c5a84f328b9652a2613080572c994

---
+++ src/main/java/com/ericsson/gerrit/plugins/highavailability/health/HealthServlet.java
@@ -0,0 +1,97 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.ericsson.gerrit.plugins.highavailability.health;
+
+import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+import static javax.servlet.http.HttpServletResponse.SC_NO_CONTENT;
+import static javax.servlet.http.HttpServletResponse.SC_SERVICE_UNAVAILABLE;
+
+import com.google.gerrit.extensions.annotations.PluginData;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class HealthServlet extends HttpServlet {
+  private static final Logger log = LoggerFactory.getLogger(HealthServlet.class);
+  private static final long serialVersionUID = -1L;
+
+  private final File unhealthyFile;
+
+  @Inject
+  HealthServlet(@PluginData Path pluginDataDir) {
+    unhealthyFile = pluginDataDir.resolve("unhealthy.txt").toFile();
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse rsp) {
+    try {
+      setHealthy();
+      rsp.setStatus(SC_NO_CONTENT);
+    } catch (IOException e) {
+      log.error("Failed to set healthy", e);
+      sendError(rsp, SC_INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @Override
+  protected void doDelete(HttpServletRequest req, HttpServletResponse rsp) {
+    try {
+      setUnhealthy();
+      rsp.setStatus(SC_NO_CONTENT);
+    } catch (IOException e) {
+      log.error("Failed to set unhealthy", e);
+      sendError(rsp, SC_INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse rsp) {
+    if (unhealthyFile.exists()) {
+      sendError(rsp, SC_SERVICE_UNAVAILABLE);
+    }
+    rsp.setStatus(SC_NO_CONTENT);
+  }
+
+  private void sendError(HttpServletResponse rsp, int statusCode) {
+    try {
+      rsp.sendError(statusCode);
+    } catch (IOException e) {
+      log.error("Failed to send error response", e);
+    }
+  }
+
+  private void setHealthy() throws IOException {
+    if (unhealthyFile.exists()) {
+      Files.delete(unhealthyFile.toPath());
+    }
+  }
+
+  private void setUnhealthy() throws IOException {
+    if (!unhealthyFile.exists()) {
+      Files.newOutputStream(unhealthyFile.toPath(), StandardOpenOption.CREATE).close();
+    }
+  }
+}
