babec7ca5cb3c536a1deab955ffe918795ebcd28
David Pursehouse <dpursehouse@collab.net>
2017-11-30 17:17:02 +0900

    AbstractIndexRestApiServlet: Don't synchronize on AtomicIteger
    As reported by SpotBugs:
    
      This method performs synchronization an object that is an instance of
      a class from the java.util.concurrent package (or its subclasses).
      Instances of these classes have their own concurrency control mechanisms
      that are orthogonal to the synchronization provided by the Java keyword
      synchronized.
    
    Instead of a map of AtomicIntegers, use a Striped of Locks.
    
    This not only gets rid of the SpotBugs warning, but also simplifies the
    code.
    
    Helped-by: Saša Živkov <sasa.zivkov@sap.com>
    Helped-by: Ban Manes <ben.manes@gmail.com>
    Change-Id: Iaed6c1c0ca22dbee88ccdf97a38b6a2beba01f48

--- src/main/java/com/ericsson/gerrit/plugins/highavailability/forwarder/rest/AbstractIndexRestApiServlet.java
+++ src/main/java/com/ericsson/gerrit/plugins/highavailability/forwarder/rest/AbstractIndexRestApiServlet.java
@@ -21,11 +21,10 @@ import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
 import static javax.servlet.http.HttpServletResponse.SC_NO_CONTENT;
 
 import com.ericsson.gerrit.plugins.highavailability.forwarder.Context;
+import com.google.common.util.concurrent.Striped;
 import com.google.gwtorm.server.OrmException;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -35,9 +34,10 @@ import org.slf4j.LoggerFactory;
 public abstract class AbstractIndexRestApiServlet<T> extends HttpServlet {
   private static final long serialVersionUID = -1L;
   private static final Logger logger = LoggerFactory.getLogger(AbstractIndexRestApiServlet.class);
-  private final Map<T, AtomicInteger> idLocks = new HashMap<>();
+
   private final String type;
   private final boolean allowDelete;
+  private final Striped<Lock> idLocks;
 
   enum Operation {
     INDEX,
@@ -56,6 +56,7 @@ public abstract class AbstractIndexRestApiServlet<T> extends HttpServlet {
   AbstractIndexRestApiServlet(String type, boolean allowDelete) {
     this.type = type;
     this.allowDelete = allowDelete;
+    this.idLocks = Striped.lock(10);
   }
 
   AbstractIndexRestApiServlet(String type) {
@@ -84,12 +85,12 @@ public abstract class AbstractIndexRestApiServlet<T> extends HttpServlet {
     logger.debug("{} {} {}", operation, type, id);
     try {
       Context.setForwardedEvent(true);
-      AtomicInteger idLock = getAndIncrementIdLock(id);
-      synchronized (idLock) {
+      Lock idLock = getLock(id);
+      idLock.lock();
+      try {
         index(id, operation);
-      }
-      if (idLock.decrementAndGet() == 0) {
-        removeIdLock(id);
+      } finally {
+        idLock.unlock();
       }
       rsp.setStatus(SC_NO_CONTENT);
     } catch (IOException e) {
@@ -104,25 +105,8 @@ public abstract class AbstractIndexRestApiServlet<T> extends HttpServlet {
     }
   }
 
-  private AtomicInteger getAndIncrementIdLock(T id) {
-    synchronized (idLocks) {
-      AtomicInteger lock = idLocks.get(id);
-      if (lock == null) {
-        lock = new AtomicInteger(1);
-        idLocks.put(id, lock);
-      } else {
-        lock.incrementAndGet();
-      }
-      return lock;
-    }
-  }
-
-  private void removeIdLock(T id) {
-    synchronized (idLocks) {
-      if (idLocks.get(id).get() == 0) {
-        idLocks.remove(id);
-      }
-    }
+  private Lock getLock(T id) {
+    return idLocks.get(id);
   }
 
   private void sendError(HttpServletResponse rsp, int statusCode, String message) {
