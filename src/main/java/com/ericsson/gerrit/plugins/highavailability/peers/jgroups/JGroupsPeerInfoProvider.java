f104ab4f4d108fb894ac60af3fe5514e87523a42
Christian Halstrick <christian.halstrick@sap.com>
2017-12-21 15:25:02 +0100

    Add an adapter to force JGroups to use SLF4J for logging/tracing
    Adds a custom log factory which when configured forces JGroups to use
    SLF4J to emit logging/tracing. This can be helpful when gerrit is
    running in a runtime environment which supports SLF4J logging.
    This logging Factory is used by default for jgroups logs.
    
    To override this default factory and specify another log factory
    set the following system property:
    
    -Djgroups.logging.log_factory_class=<YourLogFactoryClass>
    
    Change-Id: I9a395c06eb5686b6deab530916327c32f7a42718

--- src/main/java/com/ericsson/gerrit/plugins/highavailability/peers/jgroups/JGroupsPeerInfoProvider.java
+++ src/main/java/com/ericsson/gerrit/plugins/highavailability/peers/jgroups/JGroupsPeerInfoProvider.java
@@ -48,6 +48,11 @@ import org.slf4j.LoggerFactory;
 public class JGroupsPeerInfoProvider extends ReceiverAdapter
     implements Provider<Optional<PeerInfo>>, LifecycleListener {
   private static final Logger log = LoggerFactory.getLogger(JGroupsPeerInfoProvider.class);
+  static {
+    if (System.getProperty(JGROUPS_LOG_FACTORY_PROPERTY) == null) {
+      System.setProperty(JGROUPS_LOG_FACTORY_PROPERTY, SLF4JLogFactory.class.getName());
+    }
+  }
 
   private final String myUrl;
   private final Configuration.PeerInfoJGroups jgroupsConfig;
@@ -56,6 +61,7 @@ public class JGroupsPeerInfoProvider extends ReceiverAdapter
   private JChannel channel;
   private Optional<PeerInfo> peerInfo = Optional.absent();
   private Address peerAddress;
+  private static final String JGROUPS_LOG_FACTORY_PROPERTY = "jgroups.logging.log_factory_class";
 
   @Inject
   JGroupsPeerInfoProvider(
