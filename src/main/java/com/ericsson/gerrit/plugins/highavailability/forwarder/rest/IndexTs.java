c6d50895523e5725041d3227b3dddffaa7ca1155
Luca Milanesio <luca.milanesio@gmail.com>
2018-02-20 15:59:34 -0800

    Auto-reindex changes, groups and accounts during startup
    Track when the last index update timestamp has been received
    and the reindex has been successful.
    
    At startup, find out if any other changes have been updated on the DB
    after the tracked 'last update timestamp' and reindex them.
    
    Change-Id: I60139e99ce048a7559413cecfa703908c461e001

---
+++ src/main/java/com/ericsson/gerrit/plugins/highavailability/forwarder/rest/IndexTs.java
@@ -0,0 +1,119 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.ericsson.gerrit.plugins.highavailability.forwarder.rest;
+
+import com.ericsson.gerrit.plugins.highavailability.Configuration;
+import com.google.gerrit.extensions.annotations.PluginData;
+import com.google.gerrit.server.git.WorkQueue;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class IndexTs {
+  private static final Logger log = LoggerFactory.getLogger(IndexTs.class);
+  private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+
+  private final Path dataDir;
+  private final Configuration cfg;
+  private final WorkQueue.Executor exec;
+  private final FlusherRunner flusher;
+
+  private volatile LocalDateTime changeTs;
+  private volatile LocalDateTime accountTs;
+  private volatile LocalDateTime groupTs;
+
+  public enum IndexName {
+    CHANGE,
+    ACCOUNT,
+    GROUP;
+
+    @Override
+    public String toString() {
+      return super.toString().toLowerCase();
+    }
+  }
+
+  class FlusherRunner implements Runnable {
+    private Map<IndexName, LocalDateTime> storedTs = new HashMap<>();
+
+    @Override
+    public void run() {
+      store(IndexName.CHANGE, changeTs);
+      store(IndexName.ACCOUNT, accountTs);
+      store(IndexName.GROUP, groupTs);
+    }
+
+    private void store(IndexName index, LocalDateTime latestTs) {
+      LocalDateTime currTs = storedTs.get(index);
+      if (currTs == null || latestTs.isAfter(currTs)) {
+        Path indexTsFile = dataDir.resolve(index.name().toLowerCase());
+        try {
+          Files.write(indexTsFile, latestTs.format(formatter).getBytes());
+          storedTs.put(index, currTs);
+        } catch (IOException e) {
+          log.error("Unable to update last timestamp for index " + index, e);
+        }
+      }
+    }
+  }
+
+  @Inject
+  public IndexTs(@PluginData Path dataDir, Configuration cfg, WorkQueue queue) {
+    this.dataDir = dataDir;
+    this.cfg = cfg;
+    this.exec = queue.getDefaultQueue();
+    this.flusher = new FlusherRunner();
+  }
+
+  public void update(IndexName index, LocalDateTime dateTime) {
+    if (cfg.autoReindex().enabled()) {
+      switch (index) {
+        case CHANGE:
+          changeTs = dateTime;
+          break;
+        case ACCOUNT:
+          accountTs = dateTime;
+          break;
+        case GROUP:
+          groupTs = dateTime;
+          break;
+      }
+      exec.execute(flusher);
+    }
+  }
+
+  public Optional<LocalDateTime> getUpdateTs(IndexName index) {
+    try {
+      Path indexTsFile = dataDir.resolve(index.name().toLowerCase());
+      if (indexTsFile.toFile().exists()) {
+        String tsString = Files.readAllLines(indexTsFile).get(0);
+        return Optional.of(LocalDateTime.parse(tsString, formatter));
+      }
+    } catch (Exception e) {
+      log.warn("Unable to read last timestamp for index {}", index);
+    }
+    return Optional.empty();
+  }
+}
