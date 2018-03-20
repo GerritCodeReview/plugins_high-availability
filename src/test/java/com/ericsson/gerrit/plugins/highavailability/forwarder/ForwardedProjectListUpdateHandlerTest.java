1064704fdada3601274c464c527f8bab69aee323
Hugo Arès <hugo.ares@ericsson.com>
2018-03-20 10:32:35 -0400

    WIP: Update project list cache instead of evicting it
    Change-Id: I4a6d90f73d32ef5324d21ab40c5623812eff18f6

---
+++ src/test/java/com/ericsson/gerrit/plugins/highavailability/forwarder/ForwardedProjectListUpdateHandlerTest.java
@@ -0,0 +1,144 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.ericsson.gerrit.plugins.highavailability.forwarder;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.verify;
+
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.project.ProjectCache;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ForwardedProjectListUpdateHandlerTest {
+
+  private static final String PROJECT_NAME = "someProject";
+  private static final Project.NameKey PROJECT_KEY = new Project.NameKey(PROJECT_NAME);
+  @Rule public ExpectedException exception = ExpectedException.none();
+  @Mock private ProjectCache projectCacheMock;
+  private ForwardedProjectListUpdateHandler handler;
+
+  @Before
+  public void setUp() throws Exception {
+    handler = new ForwardedProjectListUpdateHandler(projectCacheMock);
+  }
+
+  @Test
+  public void testSuccessfulAdd() throws Exception {
+    handler.add(PROJECT_NAME);
+    verify(projectCacheMock).onCreateProject(PROJECT_KEY);
+  };
+
+  @Test
+  public void testSuccessfulRemove() throws Exception {
+    handler.remove(PROJECT_NAME);
+    verify(projectCacheMock).remove(PROJECT_KEY);
+  };
+
+  @Test
+  public void shouldSetAndUnsetForwardedContextOnAdd() throws Exception {
+    //this doAnswer is to allow to assert that context is set to forwarded
+    //while cache eviction is called.
+    doAnswer(
+            (Answer<Void>)
+                invocation -> {
+                  assertThat(Context.isForwardedEvent()).isTrue();
+                  return null;
+                })
+        .when(projectCacheMock)
+        .onCreateProject(PROJECT_KEY);
+
+    assertThat(Context.isForwardedEvent()).isFalse();
+    handler.add(PROJECT_NAME);
+    assertThat(Context.isForwardedEvent()).isFalse();
+
+    verify(projectCacheMock).onCreateProject(PROJECT_KEY);
+  }
+
+  @Test
+  public void shouldSetAndUnsetForwardedContextOnRemove() throws Exception {
+    //this doAnswer is to allow to assert that context is set to forwarded
+    //while cache eviction is called.
+    doAnswer(
+            (Answer<Void>)
+                invocation -> {
+                  assertThat(Context.isForwardedEvent()).isTrue();
+                  return null;
+                })
+        .when(projectCacheMock)
+        .remove(PROJECT_KEY);
+
+    assertThat(Context.isForwardedEvent()).isFalse();
+    handler.remove(PROJECT_NAME);
+    assertThat(Context.isForwardedEvent()).isFalse();
+
+    verify(projectCacheMock).remove(PROJECT_KEY);
+  }
+
+  @Test
+  public void shouldSetAndUnsetForwardedContextEvenIfExceptionIsThrownOnAdd() throws Exception {
+    doAnswer(
+            (Answer<Void>)
+                invocation -> {
+                  assertThat(Context.isForwardedEvent()).isTrue();
+                  throw new RuntimeException("someMessage");
+                })
+        .when(projectCacheMock)
+        .onCreateProject(PROJECT_KEY);
+
+    assertThat(Context.isForwardedEvent()).isFalse();
+    try {
+      handler.add(PROJECT_NAME);
+      fail("should have thrown a RuntimeException");
+    } catch (RuntimeException e) {
+      assertThat(e.getMessage()).isEqualTo("someMessage");
+    }
+    assertThat(Context.isForwardedEvent()).isFalse();
+
+    verify(projectCacheMock).onCreateProject(PROJECT_KEY);
+  }
+
+  @Test
+  public void shouldSetAndUnsetForwardedContextEvenIfExceptionIsThrownOnRemove() throws Exception {
+    doAnswer(
+            (Answer<Void>)
+                invocation -> {
+                  assertThat(Context.isForwardedEvent()).isTrue();
+                  throw new RuntimeException("someMessage");
+                })
+        .when(projectCacheMock)
+        .remove(PROJECT_KEY);
+
+    assertThat(Context.isForwardedEvent()).isFalse();
+    try {
+      handler.remove(PROJECT_NAME);
+      fail("should have thrown a RuntimeException");
+    } catch (RuntimeException e) {
+      assertThat(e.getMessage()).isEqualTo("someMessage");
+    }
+    assertThat(Context.isForwardedEvent()).isFalse();
+
+    verify(projectCacheMock).remove(PROJECT_KEY);
+  }
+}
