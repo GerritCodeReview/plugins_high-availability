c07892eab72519f63e72706787876bc67c9360e7
Luca Milanesio <luca.milanesio@gmail.com>
2018-12-29 13:18:14 +0000

    ProjectIndex: support project index propagation
    Forward events about project indexing across all the nodes
    in the high-availability cluster.
    
    Change-Id: I310d0c744dd2f963d56302c23fb257eb63a562e6

--- src/main/java/com/ericsson/gerrit/plugins/highavailability/forwarder/Forwarder.java
+++ src/main/java/com/ericsson/gerrit/plugins/highavailability/forwarder/Forwarder.java
@@ -57,6 +57,14 @@ public interface Forwarder {
   boolean indexGroup(String uuid, IndexEvent indexEvent);
 
   /**
+   * Forward a project indexing event to the other master.
+   *
+   * @param projectName the project to index.
+   * @return true if successful, otherwise false.
+   */
+  boolean indexProject(String projectName);
+
+  /**
    * Forward a stream event to the other master.
    *
    * @param event the event to forward.
