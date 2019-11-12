4f75712c827c8c219382d7ac474dbe9ca2ab4888
Luca Milanesio <luca.milanesio@gmail.com>
2019-11-12 04:56:01 -0800

    Docker tests: Initialize upon missing keys
    On Gerrit v3.x the SSH keys are not in the Docker container
    and thus the init step needs to be run on all nodes for
    triggering the keys initialisation.
    
    Change-Id: I59ab640079e2abcfc3c41ed15e542c320166d1c1

--- src/test/docker/gerrit/start.sh
+++ src/test/docker/gerrit/start.sh
@@ -9,7 +9,7 @@ fi
 
 sudo -u gerrit cp /var/gerrit/etc/gerrit.config.orig /var/gerrit/etc/gerrit.config
 
-if [[ ! -f /var/gerrit/git/All-Projects.git/config ]]
+if [[ ! -f /var/gerrit/etc/ssh_host_ed25519_key ]]
 then
   echo "Initializing Gerrit site ..."
   sudo -u gerrit java -jar /var/gerrit/bin/gerrit.war init -d /var/gerrit --batch
