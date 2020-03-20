95101845dab43fb52a4df70fbcbb7110782fae1b
Marco Miller <marco.miller@ericsson.com>
2020-03-20 12:08:02 -0400

    e2e-tests: Adapt json filename constant to new name
    As that constant is now reused by both core and non-core test scenarios.
    
    Since the constant is being renamed in the core framework, refactor its
    name accordingly in these plugin scenario classes, too.
    
    Change-Id: Ib6bc70c960db6a3bb588c796e2fab40f0f31a697
    (cherry picked from commit c15dc6ca65cb0c2978ee0b9dcdc04994ef0ee242)

--- src/test/scala/com/ericsson/gerrit/plugins/highavailability/scenarios/CloneUsingHAGerrit2.scala
+++ src/test/scala/com/ericsson/gerrit/plugins/highavailability/scenarios/CloneUsingHAGerrit2.scala
@@ -22,7 +22,7 @@ import io.gatling.core.structure.ScenarioBuilder
 import scala.concurrent.duration._
 
 class CloneUsingHAGerrit2 extends GitSimulation {
-  private val data: FileBasedFeederBuilder[Any]#F = jsonFile(added).queue
+  private val data: FileBasedFeederBuilder[Any]#F = jsonFile(resource).queue
 
   private val test: ScenarioBuilder = scenario(name)
     .feed(data)
