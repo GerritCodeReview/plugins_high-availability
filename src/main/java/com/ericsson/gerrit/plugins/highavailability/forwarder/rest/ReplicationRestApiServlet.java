61f6407cb66a7cb36b478aec8500f6085f6532dc
Nguyen Tuan Khang Phan <nguyen.tuan.khang.phan@ericsson.com>
2020-03-30 12:07:31 -0400

    Initiate the replication offloading feature
    This is the first commit in the series that introduces the replication
    offloading feature to the high-availability plugin. This feature allows
    offloading replication tasks from the active master to passive master by
    synchronizing project events between active and passive instances.
    
    Now that project events are synchronized, the active master can have its
    replication plugin disabled to free resources that would be allocated to
    perform replication. It can be done through the usage of deployment or
    operations scripts due to high-availability plugin being agnostic to
    active/passive master states. Upon receiving forwarded project events
    from the active master, the passive instance will perform replication.
    This offloading feature is inspired by events forwarding that is already
    supported by the high-availability plugin.
    
    In the end, actual replication is performed by the replication plugin
    itself. High-availability plugin only 1. synchronizes events (including
    project events) between masters and 2. triggers replication (if it is a
    passive master).
    
    This change implements a "replication" listener to listen to
    ProjectEvents that are supported by replication (RefUpdated,
    NewProjectCreated, HeadUpdated, ProjectDeleted). ReplicationListener
    schedules ProjectEvents to be forwarded to the passive instance.
    
    Add forwarding of ProjectEvents in RestForwarder and implement the
    servlet to receive those events. Replication handling and triggering
    will be introduced in a follow-up commit.
    
    Change-Id: I5a75addc56541cd878eb73ca107f741ba5129098

---
+++ src/main/java/com/ericsson/gerrit/plugins/highavailability/forwarder/rest/ReplicationRestApiServlet.java
@@ -0,0 +1,76 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.ericsson.gerrit.plugins.highavailability.forwarder.rest;
+
+import static com.google.common.net.MediaType.JSON_UTF_8;
+import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+import static javax.servlet.http.HttpServletResponse.SC_NO_CONTENT;
+import static javax.servlet.http.HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE;
+
+import com.ericsson.gerrit.plugins.highavailability.replication.events.Deserializer;
+import com.google.common.flogger.FluentLogger;
+import com.google.common.io.CharStreams;
+import com.google.common.net.MediaType;
+import com.google.gerrit.extensions.events.ProjectEvent;
+import com.google.gerrit.extensions.restapi.Url;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Singleton
+class ReplicationRestApiServlet extends AbstractRestApiServlet {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+  private static final long serialVersionUID = -1L;
+
+  @Inject
+  ReplicationRestApiServlet() {}
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse rsp) {
+    setHeaders(rsp);
+    try {
+      if (!MediaType.parse(req.getContentType()).is(JSON_UTF_8)) {
+        sendError(rsp, SC_UNSUPPORTED_MEDIA_TYPE, "Expecting " + JSON_UTF_8 + " content type");
+        return;
+      }
+
+      ProjectEvent receivedEvent = getEventFromRequest(req);
+      logger.atInfo().log(
+          "Received project event "
+              + receivedEvent.getClass().getSimpleName()
+              + " for project "
+              + receivedEvent.getProjectName());
+      rsp.setStatus(SC_NO_CONTENT);
+    } catch (IOException e) {
+      logger.atSevere().log("Unable decode ProjectEvent", e);
+      sendError(rsp, SC_BAD_REQUEST, e.getMessage());
+    }
+  }
+
+  private static ProjectEvent getEventFromRequest(HttpServletRequest req) throws IOException {
+    String jsonEvent = CharStreams.toString(req.getReader());
+    String requestURI = req.getRequestURI();
+    String className = Url.decode(requestURI.substring(requestURI.lastIndexOf('/') + 1));
+    Gson gson =
+        new GsonBuilder()
+            .registerTypeAdapter(ProjectEvent.class, new Deserializer(className))
+            .create();
+    return gson.fromJson(jsonEvent, ProjectEvent.class);
+  }
+}
