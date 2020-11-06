32f5464aa9f7f0db41554d4a46ea206f21711255
Marcin Czech <maczech@gmail.com>
2020-11-06 13:56:38 +0100

    Avoid multiple indexing operation for a single change
    Before this code change, if active node calls passive node to index
    some id and there already was an indexing operation for that id, we
    ended up with multiple concurrent indexing operations. Adding tracking
    of in-flight indexing operations allows avoiding multiple indexing
    for the same id. Set of in-flight ids instead of locking fixes the
    issue with occupying receivers threads while waiting for a lock.
    
    Feature: Issue 13637
    Change-Id: I660e9b9f9879beb6ca44103f96dc194fd2bcaa7e

--- src/main/java/com/ericsson/gerrit/plugins/highavailability/forwarder/ForwardedIndexingHandler.java
+++ src/main/java/com/ericsson/gerrit/plugins/highavailability/forwarder/ForwardedIndexingHandler.java
@@ -16,7 +16,10 @@ package com.ericsson.gerrit.plugins.highavailability.forwarder;
 
 import com.google.common.flogger.FluentLogger;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Base class to handle forwarded indexing. This class is meant to be extended by classes used on
@@ -26,6 +29,7 @@ import java.util.Optional;
  */
 public abstract class ForwardedIndexingHandler<T> {
   protected static final FluentLogger log = FluentLogger.forEnclosingClass();
+  private final Set<T> inFlightIndexing = Collections.newSetFromMap(new ConcurrentHashMap<>());
 
   public enum Operation {
     INDEX,
@@ -51,21 +55,27 @@ public abstract class ForwardedIndexingHandler<T> {
    */
   public void index(T id, Operation operation, Optional<IndexEvent> indexEvent) throws IOException {
     log.atFine().log("%s %s %s", operation, id, indexEvent);
-    try {
-      Context.setForwardedEvent(true);
-      switch (operation) {
-        case INDEX:
-          doIndex(id, indexEvent);
-          break;
-        case DELETE:
-          doDelete(id, indexEvent);
-          break;
-        default:
-          log.atSevere().log("unexpected operation: %s", operation);
-          break;
+    if (inFlightIndexing.add(id)) {
+      try {
+        Context.setForwardedEvent(true);
+        switch (operation) {
+          case INDEX:
+            doIndex(id, indexEvent);
+            break;
+          case DELETE:
+            doDelete(id, indexEvent);
+            break;
+          default:
+            log.atSevere().log("unexpected operation: %s", operation);
+            break;
+        }
+      } finally {
+        Context.unsetForwardedEvent();
+        inFlightIndexing.remove(id);
       }
-    } finally {
-      Context.unsetForwardedEvent();
+    } else {
+      throw new InFlightIndexedException(
+          String.format("Indexing for %s %s %s already in flight", operation, id, indexEvent));
     }
   }
 }
