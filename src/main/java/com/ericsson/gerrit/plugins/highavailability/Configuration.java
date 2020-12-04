4b2c0a7bca770c54a213a62c3b581c45ead5fd17
Marcin Czech <maczech@gmail.com>
2020-12-04 11:45:09 +0100

    Remove index.waitTimeout configuration property
    Lock acquisition timeout is set to 5 milliseconds. Increasing the
    timeout causes healthcheck plugin to report temporary deadlock and
    mark the entire node as unhealthy.
    For that reason there is no point to increase timeout to more than
    a few milliseconds.
    
    Use `http.socketTimeout` and `http.maxTries` to control for how long
    the plugin will keep retrying to acquire the lock.
    
    Feature: Issue 13561
    Change-Id: I01d6c42a0f9ac801a52b9e5915502d0eea54f134

--- src/main/java/com/ericsson/gerrit/plugins/highavailability/Configuration.java
+++ src/main/java/com/ericsson/gerrit/plugins/highavailability/Configuration.java
@@ -455,7 +455,6 @@ public class Configuration {
     private final int threadPoolSize;
     private final int retryInterval;
     private final int maxTries;
-    private final int waitTimeout;
     private final int numStripedLocks;
     private final boolean synchronizeForced;
 
@@ -465,7 +464,6 @@ public class Configuration {
       numStripedLocks = getInt(cfg, INDEX_SECTION, NUM_STRIPED_LOCKS, DEFAULT_NUM_STRIPED_LOCKS);
       retryInterval = getInt(cfg, INDEX_SECTION, RETRY_INTERVAL_KEY, DEFAULT_INDEX_RETRY_INTERVAL);
       maxTries = getInt(cfg, INDEX_SECTION, MAX_TRIES_KEY, DEFAULT_INDEX_MAX_TRIES);
-      waitTimeout = getInt(cfg, INDEX_SECTION, WAIT_TIMEOUT_KEY, DEFAULT_TIMEOUT_MS);
       synchronizeForced =
           cfg.getBoolean(INDEX_SECTION, SYNCHRONIZE_FORCED_KEY, DEFAULT_SYNCHRONIZE_FORCED);
     }
@@ -484,10 +482,6 @@ public class Configuration {
 
     public int maxTries() {
       return maxTries;
-    }
-
-    public int waitTimeout() {
-      return waitTimeout;
     }
 
     public boolean synchronizeForced() {
