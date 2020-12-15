268d6b3aa1de52512b51adcc4b0d77de7b66fe6b
Nguyen Tuan Khang Phan <nguyen.tuan.khang.phan@ericsson.com>
2020-12-15 09:21:56 -0500

    Stop broadcasting accounts cache eviction
    The solution is based on multi-site plugin fix: Iaf3f9c0b944 which is
    the sibling of high-availability plugin. To adapt
    CacheEvictionHandlerTest access modifier for Configuration was set to
    public with @VisibleForTesting tag.
    
    Bug: Issue 13811
    Change-Id: Iaf3f9c0b9440d234a83f9ed7d976ca7368c31aaa

--- src/test/java/com/ericsson/gerrit/plugins/highavailability/cache/CachePatternMatcherTest.java
+++ src/test/java/com/ericsson/gerrit/plugins/highavailability/cache/CachePatternMatcherTest.java
@@ -38,7 +38,6 @@ public class CachePatternMatcherTest {
     CachePatternMatcher matcher = new CachePatternMatcher(configurationMock);
     for (String cache :
         ImmutableList.of(
-            "accounts",
             "groups",
             "groups_byinclude",
             "groups_byname",
@@ -55,6 +54,7 @@ public class CachePatternMatcherTest {
     }
     for (String cache :
         ImmutableList.of(
+            "accounts",
             "adv_bases",
             "change_kind",
             "change_notes",
