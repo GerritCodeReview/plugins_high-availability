04c3b492f9275c6e2d745cfac873c77a663822b8
Antonio Barone <syntonyze@gmail.com>
2021-04-08 17:06:15 +0200

    Honour index retries when indexing groups
    Similarly to the ForwardedIndexChangeHandler, the
    ForwardedGroupChangeHandler will attempt multiple reindexes up to
    `index.maxTries`, every `index.retryInterval` milliseconds.
    
    To be able to understand whether replication is completed and the local
    group ref is up-to-date, two main changes have been introduced:
    
    - A GroupChecker class, with the responsibility to lookup in All-Users
      for the status of a specified group
    - Populate the 'targetSha' field of the IndexEvent class, to indicate
      which particular sha1 the ref group was updated to, so that
      GroupChecker could perform a specific lookup.
    
    This change was cherry-picked and adapted from the equivalent multi-site
    fix done in Ifc171a49a0.
    
    Bug: Issue 14349
    Change-Id: I0843f492c1dd112a63667132bb2af73028f411a5

--- src/main/java/com/ericsson/gerrit/plugins/highavailability/forwarder/ForwardedIndexGroupHandler.java
+++ src/main/java/com/ericsson/gerrit/plugins/highavailability/forwarder/ForwardedIndexGroupHandler.java
@@ -14,11 +14,17 @@
 
 package com.ericsson.gerrit.plugins.highavailability.forwarder;
 
+import com.ericsson.gerrit.plugins.highavailability.Configuration;
+import com.ericsson.gerrit.plugins.highavailability.index.ForwardedIndexExecutor;
+import com.ericsson.gerrit.plugins.highavailability.index.GroupChecker;
 import com.google.gerrit.entities.AccountGroup;
 import com.google.gerrit.server.index.group.GroupIndexer;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import java.util.Optional;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Index a group using {@link GroupIndexer}. This class is meant to be used on the receiving side of
@@ -29,16 +35,72 @@ import java.util.Optional;
 @Singleton
 public class ForwardedIndexGroupHandler extends ForwardedIndexingHandler<AccountGroup.UUID> {
   private final GroupIndexer indexer;
+  private final GroupChecker groupChecker;
+  private final ScheduledExecutorService indexExecutor;
+  private final int retryInterval;
+  private final int maxTries;
 
   @Inject
-  ForwardedIndexGroupHandler(GroupIndexer indexer) {
+  ForwardedIndexGroupHandler(
+      GroupIndexer indexer,
+      GroupChecker groupChecker,
+      Configuration config,
+      @ForwardedIndexExecutor ScheduledExecutorService indexExecutor) {
     this.indexer = indexer;
+    this.groupChecker = groupChecker;
+    this.indexExecutor = indexExecutor;
+    Configuration.Index indexConfig = config.index();
+    this.retryInterval = indexConfig != null ? indexConfig.retryInterval() : 0;
+    this.maxTries = indexConfig != null ? indexConfig.maxTries() : 0;
   }
 
   @Override
   protected void doIndex(AccountGroup.UUID uuid, Optional<IndexEvent> indexEvent) {
+    doIndex(uuid, indexEvent, 0);
+  }
+
+  protected void doIndex(
+      AccountGroup.UUID uuid, Optional<IndexEvent> groupIndexEvent, int retryCount) {
     indexer.index(uuid);
-    log.atFine().log("Group %s successfully indexed", uuid);
+    if (groupChecker.isGroupUpToDate(uuid, groupIndexEvent)) {
+      if (retryCount > 0) {
+        log.atWarning().log(
+            "Group '%s' has been eventually indexed after %s attempt(s)", uuid, retryCount);
+      } else {
+        log.atFine().log("Group '%s' successfully indexed", uuid);
+      }
+    } else {
+      log.atFine().log("Group '%s' rescheduling indexing", uuid);
+      rescheduleIndex(uuid, groupIndexEvent, retryCount + 1);
+    }
+  }
+
+  private boolean rescheduleIndex(
+      AccountGroup.UUID uuid, Optional<IndexEvent> groupIndexEvent, int retryCount) {
+    if (retryCount > maxTries) {
+      log.atSevere().log(
+          "Group '%s' could not be indexed after %s retries. Group index could be stale.",
+          uuid, retryCount);
+      return false;
+    }
+
+    log.atWarning().log(
+        "Retrying for the #%s time to index Group %s after %s msecs",
+        retryCount, uuid, retryInterval);
+    @SuppressWarnings("unused")
+    Future<?> possiblyIgnoredError =
+        indexExecutor.schedule(
+            () -> {
+              try {
+                Context.setForwardedEvent(true);
+                doIndex(uuid, groupIndexEvent, retryCount);
+              } catch (Exception e) {
+                log.atWarning().withCause(e).log("Group %s could not be indexed", uuid);
+              }
+            },
+            retryInterval,
+            TimeUnit.MILLISECONDS);
+    return true;
   }
 
   @Override
