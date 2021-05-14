bf6dcb1ae417efec39ca2b1ae35c161aa9668eaa
Przemyslaw Waliszewski <pwaliszewski@gmail.com>
2021-05-14 19:35:44 +0200

    Add support for indexIfNeeded for Projects
    Without this change autoreindexing doesn't work for projects.
    Change requires adding support for lastUpdatedOn to gerrit core.
    
    Bug: Issue 14223
    Change-Id: I4829ba44b177cd6080988797122142d708069cf1

--- src/main/java/com/ericsson/gerrit/plugins/highavailability/autoreindex/ProjectReindexRunnable.java
+++ src/main/java/com/ericsson/gerrit/plugins/highavailability/autoreindex/ProjectReindexRunnable.java
@@ -14,24 +14,37 @@
 
 package com.ericsson.gerrit.plugins.highavailability.autoreindex;
 
+import com.ericsson.gerrit.plugins.highavailability.forwarder.ForwardedIndexProjectHandler;
+import com.ericsson.gerrit.plugins.highavailability.forwarder.ForwardedIndexingHandler.Operation;
 import com.ericsson.gerrit.plugins.highavailability.forwarder.rest.AbstractIndexRestApiServlet;
+import com.google.common.flogger.FluentLogger;
 import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.reviewdb.server.ReviewDb;
 import com.google.gerrit.server.project.ProjectCache;
 import com.google.gerrit.server.util.OneOffRequestContext;
+import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
+import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.Objects;
 import java.util.Optional;
 
 public class ProjectReindexRunnable extends ReindexRunnable<Project.NameKey> {
 
+  private static final FluentLogger log = FluentLogger.forEnclosingClass();
+
   private final ProjectCache projectCache;
+  private final ForwardedIndexProjectHandler indexer;
 
   @Inject
   public ProjectReindexRunnable(
-      IndexTs indexTs, OneOffRequestContext ctx, ProjectCache projectCache) {
+      ForwardedIndexProjectHandler indexer,
+      IndexTs indexTs,
+      OneOffRequestContext ctx,
+      ProjectCache projectCache) {
     super(AbstractIndexRestApiServlet.IndexName.PROJECT, indexTs, ctx);
     this.projectCache = projectCache;
+    this.indexer = indexer;
   }
 
   @Override
@@ -41,6 +54,15 @@ public class ProjectReindexRunnable extends ReindexRunnable<Project.NameKey> {
 
   @Override
   protected Optional<Timestamp> indexIfNeeded(ReviewDb db, Project.NameKey g, Timestamp sinceTs) {
+    Timestamp timestamp = projectCache.get(g).getProject().getLastUpdatedOn();
+    if (Objects.nonNull(timestamp) && timestamp.after(sinceTs)) {
+      try {
+        indexer.index(g, Operation.INDEX, Optional.empty());
+      } catch (IOException | OrmException e) {
+        log.atSevere().withCause(e).log("Reindex failed");
+      }
+      return Optional.of(sinceTs);
+    }
     return Optional.empty();
   }
 }
