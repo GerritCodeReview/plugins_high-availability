c6e193487e7a2826851659acd553f2ad4b3a4cf2
zoleoho <oleksandr.ohorodnik@tietoevry.com>
2022-06-15 00:22:58 +0300

    Cover source code of high-availability plugin jgroups mode with tests
    Feature: Issue 10020
    Change-Id: Icc11d4287c64be10d3bcebed73c8bcb627886d85

--- src/test/java/com/ericsson/gerrit/plugins/highavailability/peers/jgroups/InetAddressFinderTest.java
+++ src/test/java/com/ericsson/gerrit/plugins/highavailability/peers/jgroups/InetAddressFinderTest.java
@@ -16,10 +16,20 @@ package com.ericsson.gerrit.plugins.highavailability.peers.jgroups;
 
 import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.Answers.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.ericsson.gerrit.plugins.highavailability.Configuration;
 import com.google.common.collect.ImmutableList;
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -32,11 +42,74 @@ public class InetAddressFinderTest {
   @Mock(answer = RETURNS_DEEP_STUBS)
   private Configuration configuration;
 
+  @Mock private NetworkInterface mockInterface;
+
   private InetAddressFinder finder;
+  private List<NetworkInterface> testNetworkInterfaces;
 
   @Before
   public void setUp() {
     finder = new InetAddressFinder(configuration);
+    testNetworkInterfaces = new ArrayList<>();
+  }
+
+  @Test
+  public void testNoSuitableInterfaceWhenFindFirstAppropriateAddress() throws SocketException {
+    when(mockInterface.isLoopback()).thenReturn(true);
+    when(configuration.jgroups().skipInterface()).thenReturn(ImmutableList.of("mockInterface1"));
+    testNetworkInterfaces.add(mockInterface);
+    assertThat(finder.findFirstAppropriateAddress(testNetworkInterfaces).isPresent()).isFalse();
+  }
+
+  @Test
+  public void testOptionalEmptyIsReturnedWhenFindFirstAppropriateAddress() throws SocketException {
+    when(mockInterface.isLoopback()).thenReturn(false);
+    when(mockInterface.isUp()).thenReturn(true);
+    when(mockInterface.supportsMulticast()).thenReturn(true);
+    when(configuration.jgroups().skipInterface()).thenReturn(ImmutableList.of());
+    testNetworkInterfaces.add(mockInterface);
+    Enumeration mockInetAddresses = mock(Enumeration.class);
+
+    when(mockInterface.getInetAddresses()).thenReturn(mockInetAddresses);
+    assertThat(finder.findFirstAppropriateAddress(testNetworkInterfaces))
+        .isEqualTo(Optional.empty());
+  }
+
+  @Test
+  public void testInet6AddressIsReturnedWhenFindFirstAppropriateAddress() throws SocketException {
+    when(mockInterface.isLoopback()).thenReturn(false);
+    when(mockInterface.isUp()).thenReturn(true);
+    when(mockInterface.supportsMulticast()).thenReturn(true);
+    when(configuration.jgroups().skipInterface()).thenReturn(ImmutableList.of());
+    testNetworkInterfaces.add(mockInterface);
+    Inet6Address mockInet6Address = mock(Inet6Address.class);
+    List<Inet6Address> mocklist = new ArrayList<>();
+    mocklist.add(mockInet6Address);
+    Enumeration mockInetAddresses = Collections.enumeration(mocklist);
+
+    when(mockInterface.getInetAddresses()).thenReturn(mockInetAddresses);
+    assertThat(finder.findFirstAppropriateAddress(testNetworkInterfaces))
+        .isEqualTo(Optional.of(mockInet6Address));
+  }
+
+  @Test
+  public void testInet4AddressIsReturnedWhenFindFirstAppropriateAddress() throws SocketException {
+    when(mockInterface.isLoopback()).thenReturn(false);
+    when(mockInterface.isUp()).thenReturn(true);
+    when(mockInterface.supportsMulticast()).thenReturn(true);
+    when(configuration.jgroups().skipInterface()).thenReturn(ImmutableList.of());
+    System.setProperty("java.net.preferIPv4Stack", "true");
+    testNetworkInterfaces.add(mockInterface);
+    Inet4Address mockInet4Address = mock(Inet4Address.class);
+    List<Inet4Address> mocklist = new ArrayList<>();
+    mocklist.add(mockInet4Address);
+
+    Enumeration mockInetAddresses = Collections.enumeration(mocklist);
+    when(mockInterface.getInetAddresses()).thenReturn(mockInetAddresses);
+
+    finder = new InetAddressFinder(configuration);
+    assertThat(finder.findFirstAppropriateAddress(testNetworkInterfaces))
+        .isEqualTo(Optional.of(mockInet4Address));
   }
 
   @Test
