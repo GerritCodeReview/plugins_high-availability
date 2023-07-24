f4a0c3e26d3d0dbb3f82cc7a458aa50fdc607d16
Saša Živkov <sasa.zivkov@sap.com>
2023-07-24 10:40:40 +0200

    Active/active setup using zookeeper-refdb plugin and zookeeper service
    Change-Id: I3e23c07239e4e034b45827a79120d33283cf045c

--- src/test/docker/gerrit/entrypoint.sh
+++ src/test/docker/gerrit/entrypoint.sh
@@ -8,6 +8,7 @@ fi
 chown -R gerrit /var/gerrit/etc
 sudo -u gerrit cp /var/gerrit/etc/gerrit.config.orig /var/gerrit/etc/gerrit.config
 sudo -u gerrit cp /var/gerrit/etc/high-availability.config.orig /var/gerrit/etc/high-availability.config
+sudo -u gerrit cp /var/gerrit/etc/zookeeper-refdb.config.orig /var/gerrit/etc/zookeeper-refdb.config
 
 
 echo "Init gerrit..."
