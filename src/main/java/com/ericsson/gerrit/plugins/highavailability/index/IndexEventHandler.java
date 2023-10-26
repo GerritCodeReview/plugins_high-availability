401c1e00a594caf05b9cfa622ec2430f513dd894
Saša Živkov <sasa.zivkov@sap.com>
2023-10-26 17:00:04 +0200

    Remove executor service(s) from the IndexEventHandler
    The RestForwarder already provides asynchronous request execution and
    retry so we do not really benefit from having another executor service
    on top which does the same.
    
    This also removes the queuedTasks because it would serve no purpose
    after the removal of the executor service: every indexing task is
    immediately given to the Forwarder and RestForwarder would immediately
    schedule an asynchronous request for each site in the HA cluster.
    
    Change-Id: I3276ecf9a4deb720309a318ecfefa8e3e55a403e

--- src/main/java/com/ericsson/gerrit/plugins/highavailability/index/IndexEventHandler.java
+++ src/main/java/com/ericsson/gerrit/plugins/highavailability/index/IndexEventHandler.java
@@ -17,19 +17,13 @@ package com.ericsson.gerrit.plugins.highavailability.index;
 import com.ericsson.gerrit.plugins.highavailability.forwarder.Context;
 import com.ericsson.gerrit.plugins.highavailability.forwarder.Forwarder;
 import com.ericsson.gerrit.plugins.highavailability.forwarder.IndexEvent;
-import com.google.common.base.Objects;
 import com.google.common.flogger.FluentLogger;
-import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.extensions.events.AccountIndexedListener;
 import com.google.gerrit.extensions.events.ChangeIndexedListener;
 import com.google.gerrit.extensions.events.GroupIndexedListener;
 import com.google.gerrit.extensions.events.ProjectIndexedListener;
 import com.google.inject.Inject;
-import java.util.Collections;
-import java.util.Set;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ScheduledExecutorService;
+import java.util.Optional;
 
 class IndexEventHandler
     implements ChangeIndexedListener,
@@ -37,26 +31,14 @@ class IndexEventHandler
         GroupIndexedListener,
         ProjectIndexedListener {
   private static final FluentLogger log = FluentLogger.forEnclosingClass();
-  private final ScheduledExecutorService executor;
-  private final ScheduledExecutorService batchExecutor;
   private final Forwarder forwarder;
-  private final String pluginName;
-  private final Set<IndexTask> queuedTasks = Collections.newSetFromMap(new ConcurrentHashMap<>());
   private final ChangeCheckerImpl.Factory changeChecker;
   private final CurrentRequestContext currCtx;
 
   @Inject
   IndexEventHandler(
-      @IndexExecutor ScheduledExecutorService executor,
-      @BatchIndexExecutor ScheduledExecutorService batchExecutor,
-      @PluginName String pluginName,
-      Forwarder forwarder,
-      ChangeCheckerImpl.Factory changeChecker,
-      CurrentRequestContext currCtx) {
+      Forwarder forwarder, ChangeCheckerImpl.Factory changeChecker, CurrentRequestContext currCtx) {
     this.forwarder = forwarder;
-    this.executor = executor;
-    this.batchExecutor = batchExecutor;
-    this.pluginName = pluginName;
     this.changeChecker = changeChecker;
     this.currCtx = currCtx;
   }
@@ -66,10 +48,7 @@ class IndexEventHandler
     currCtx.onlyWithContext(
         (ctx) -> {
           if (!Context.isForwardedEvent()) {
-            IndexAccountTask task = new IndexAccountTask(id);
-            if (queuedTasks.add(task)) {
-              executor.execute(task);
-            }
+            forwarder.indexAccount(id, new IndexEvent());
           }
         });
   }
@@ -80,30 +59,19 @@ class IndexEventHandler
   }
 
   private void executeIndexChangeTask(String projectName, int id) {
-
     if (!Context.isForwardedEvent()) {
       String changeId = projectName + "~" + id;
       try {
-        changeChecker
-            .create(changeId)
-            .newIndexEvent()
-            .map(
-                event -> {
-                  if (Thread.currentThread().getName().contains("Batch")) {
-                    return new BatchIndexChangeTask(projectName, id, event);
-                  }
-                  return new IndexChangeTask(projectName, id, event);
-                })
-            .ifPresent(
-                task -> {
-                  if (queuedTasks.add(task)) {
-                    if (task instanceof BatchIndexChangeTask) {
-                      batchExecutor.execute(task);
-                    } else {
-                      executor.execute(task);
-                    }
-                  }
-                });
+        Optional<IndexEvent> indexEvent = changeChecker.create(changeId).newIndexEvent();
+        if (indexEvent.isEmpty()) {
+          return;
+        }
+
+        if (Thread.currentThread().getName().contains("Batch")) {
+          forwarder.batchIndexChange(projectName, id, indexEvent.get());
+        } else {
+          forwarder.indexChange(projectName, id, indexEvent.get());
+        }
       } catch (Exception e) {
         log.atWarning().withCause(e).log("Unable to create task to reindex change %s", changeId);
       }
@@ -113,281 +81,21 @@ class IndexEventHandler
   @Override
   public void onChangeDeleted(int id) {
     if (!Context.isForwardedEvent()) {
-      DeleteChangeTask task = new DeleteChangeTask(id, new IndexEvent());
-      if (queuedTasks.add(task)) {
-        executor.execute(task);
-      }
+      forwarder.deleteChangeFromIndex(id, new IndexEvent());
     }
   }
 
   @Override
   public void onProjectIndexed(String projectName) {
     if (!Context.isForwardedEvent()) {
-      IndexProjectTask task = new IndexProjectTask(projectName);
-      if (queuedTasks.add(task)) {
-        executor.execute(task);
-      }
+      forwarder.indexProject(projectName, new IndexEvent());
     }
   }
 
   @Override
   public void onGroupIndexed(String groupUUID) {
     if (!Context.isForwardedEvent()) {
-      IndexGroupTask task = new IndexGroupTask(groupUUID);
-      if (queuedTasks.add(task)) {
-        executor.execute(task);
-      }
-    }
-  }
-
-  abstract class IndexTask implements Runnable {
-    protected final IndexEvent indexEvent;
-
-    IndexTask() {
-      indexEvent = new IndexEvent();
-    }
-
-    IndexTask(IndexEvent indexEvent) {
-      this.indexEvent = indexEvent;
-    }
-
-    @Override
-    public void run() {
-      queuedTasks.remove(this);
-      execute();
-    }
-
-    abstract CompletableFuture<Boolean> execute();
-
-    abstract String indexId();
-  }
-
-  class IndexChangeTask extends IndexTask {
-    private final int changeId;
-    private final String projectName;
-
-    IndexChangeTask(String projectName, int changeId, IndexEvent indexEvent) {
-      super(indexEvent);
-      this.projectName = projectName;
-      this.changeId = changeId;
-    }
-
-    @Override
-    public CompletableFuture<Boolean> execute() {
-      return forwarder.indexChange(projectName, changeId, indexEvent);
-    }
-
-    @Override
-    public int hashCode() {
-      return Objects.hashCode(IndexChangeTask.class, changeId);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-      if (!(obj instanceof IndexChangeTask)) {
-        return false;
-      }
-      IndexChangeTask other = (IndexChangeTask) obj;
-      return changeId == other.changeId;
-    }
-
-    @Override
-    public String toString() {
-      return String.format("[%s] Index change %s in target instance", pluginName, changeId);
-    }
-
-    @Override
-    String indexId() {
-      return "change/" + changeId;
-    }
-  }
-
-  class BatchIndexChangeTask extends IndexTask {
-    private final int changeId;
-    private final String projectName;
-
-    BatchIndexChangeTask(String projectName, int changeId, IndexEvent indexEvent) {
-      super(indexEvent);
-      this.projectName = projectName;
-      this.changeId = changeId;
-    }
-
-    @Override
-    public CompletableFuture<Boolean> execute() {
-      return forwarder.batchIndexChange(projectName, changeId, indexEvent);
-    }
-
-    @Override
-    String indexId() {
-      return "change/" + changeId;
-    }
-
-    @Override
-    public int hashCode() {
-      return Objects.hashCode(IndexEventHandler.BatchIndexChangeTask.class, changeId);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-      if (!(obj instanceof IndexEventHandler.BatchIndexChangeTask)) {
-        return false;
-      }
-      IndexEventHandler.BatchIndexChangeTask other = (IndexEventHandler.BatchIndexChangeTask) obj;
-      return changeId == other.changeId;
-    }
-
-    @Override
-    public String toString() {
-      return String.format("[%s] Index change %s in target instance", pluginName, changeId);
-    }
-  }
-
-  class DeleteChangeTask extends IndexTask {
-    private final int changeId;
-
-    DeleteChangeTask(int changeId, IndexEvent indexEvent) {
-      super(indexEvent);
-      this.changeId = changeId;
-    }
-
-    @Override
-    public CompletableFuture<Boolean> execute() {
-      return forwarder.deleteChangeFromIndex(changeId, indexEvent);
-    }
-
-    @Override
-    public int hashCode() {
-      return Objects.hashCode(DeleteChangeTask.class, changeId);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-      if (!(obj instanceof DeleteChangeTask)) {
-        return false;
-      }
-      DeleteChangeTask other = (DeleteChangeTask) obj;
-      return changeId == other.changeId;
-    }
-
-    @Override
-    public String toString() {
-      return String.format("[%s] Delete change %s in target instance", pluginName, changeId);
-    }
-
-    @Override
-    String indexId() {
-      return "change/" + changeId;
-    }
-  }
-
-  class IndexAccountTask extends IndexTask {
-    private final int accountId;
-
-    IndexAccountTask(int accountId) {
-      this.accountId = accountId;
-    }
-
-    @Override
-    public CompletableFuture<Boolean> execute() {
-      return forwarder.indexAccount(accountId, indexEvent);
-    }
-
-    @Override
-    public int hashCode() {
-      return Objects.hashCode(accountId);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-      if (!(obj instanceof IndexAccountTask)) {
-        return false;
-      }
-      IndexAccountTask other = (IndexAccountTask) obj;
-      return accountId == other.accountId;
-    }
-
-    @Override
-    public String toString() {
-      return String.format("[%s] Index account %s in target instance", pluginName, accountId);
-    }
-
-    @Override
-    String indexId() {
-      return "account/" + accountId;
-    }
-  }
-
-  class IndexGroupTask extends IndexTask {
-    private final String groupUUID;
-
-    IndexGroupTask(String groupUUID) {
-      this.groupUUID = groupUUID;
-    }
-
-    @Override
-    public CompletableFuture<Boolean> execute() {
-      return forwarder.indexGroup(groupUUID, indexEvent);
-    }
-
-    @Override
-    public int hashCode() {
-      return Objects.hashCode(IndexGroupTask.class, groupUUID);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-      if (!(obj instanceof IndexGroupTask)) {
-        return false;
-      }
-      IndexGroupTask other = (IndexGroupTask) obj;
-      return groupUUID.equals(other.groupUUID);
-    }
-
-    @Override
-    public String toString() {
-      return String.format("[%s] Index group %s in target instance", pluginName, groupUUID);
-    }
-
-    @Override
-    String indexId() {
-      return "group/" + groupUUID;
-    }
-  }
-
-  class IndexProjectTask extends IndexTask {
-    private final String projectName;
-
-    IndexProjectTask(String projectName) {
-      this.projectName = projectName;
-    }
-
-    @Override
-    public CompletableFuture<Boolean> execute() {
-      return forwarder.indexProject(projectName, indexEvent);
-    }
-
-    @Override
-    public int hashCode() {
-      return Objects.hashCode(IndexProjectTask.class, projectName);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-      if (!(obj instanceof IndexProjectTask)) {
-        return false;
-      }
-      IndexProjectTask other = (IndexProjectTask) obj;
-      return projectName.equals(other.projectName);
-    }
-
-    @Override
-    public String toString() {
-      return String.format("[%s] Index project %s in target instance", pluginName, projectName);
-    }
-
-    @Override
-    String indexId() {
-      return "project/" + projectName;
+      forwarder.indexGroup(groupUUID, new IndexEvent());
     }
   }
 }
--- src/test/java/com/ericsson/gerrit/plugins/highavailability/index/IndexEventHandlerTest.java
+++ src/test/java/com/ericsson/gerrit/plugins/highavailability/index/IndexEventHandlerTest.java
@@ -14,12 +14,10 @@
 
 package com.ericsson.gerrit.plugins.highavailability.index;
 
-import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
@@ -28,28 +26,14 @@ import com.ericsson.gerrit.plugins.highavailability.Configuration;
 import com.ericsson.gerrit.plugins.highavailability.forwarder.Context;
 import com.ericsson.gerrit.plugins.highavailability.forwarder.Forwarder;
 import com.ericsson.gerrit.plugins.highavailability.forwarder.IndexEvent;
-import com.ericsson.gerrit.plugins.highavailability.index.IndexEventHandler.DeleteChangeTask;
-import com.ericsson.gerrit.plugins.highavailability.index.IndexEventHandler.IndexAccountTask;
-import com.ericsson.gerrit.plugins.highavailability.index.IndexEventHandler.IndexChangeTask;
-import com.ericsson.gerrit.plugins.highavailability.index.IndexEventHandler.IndexGroupTask;
 import com.google.gerrit.entities.Account;
 import com.google.gerrit.entities.AccountGroup;
 import com.google.gerrit.entities.Change;
 import com.google.gerrit.server.util.OneOffRequestContext;
 import com.google.gerrit.server.util.RequestContext;
 import com.google.gerrit.server.util.ThreadLocalRequestContext;
-import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.function.Consumer;
 import org.junit.Before;
 import org.junit.Test;
@@ -59,13 +43,10 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class IndexEventHandlerTest {
-  private static final String PLUGIN_NAME = "high-availability";
   private static final String PROJECT_NAME = "test/project";
   private static final int CHANGE_ID = 1;
   private static final int ACCOUNT_ID = 2;
   private static final String UUID = "3";
-  private static final String OTHER_UUID = "4";
-  private static final String EXECUTOR_THREAD_NAME = "EXECUTOR_THREAD";
 
   private IndexEventHandler indexEventHandler;
   @Mock private Forwarder forwarder;
@@ -74,11 +55,8 @@ public class IndexEventHandlerTest {
   private Change.Id changeId;
   private Account.Id accountId;
   private AccountGroup.UUID accountGroupUUID;
-  private ScheduledExecutorService executor = new CurrentThreadScheduledExecutorService();
-  private ScheduledExecutorService batchExecutor = new CurrentThreadScheduledExecutorService();
   @Mock private RequestContext mockCtx;
   @Mock private Configuration configuration;
-  private Thread executorThread;
 
   private CurrentRequestContext currCtx =
       new CurrentRequestContext(null, null, null) {
@@ -108,15 +86,12 @@ public class IndexEventHandlerTest {
   }
 
   public void setUpIndexEventHandler(CurrentRequestContext currCtx) throws Exception {
-    indexEventHandler =
-        new IndexEventHandler(
-            executor, batchExecutor, PLUGIN_NAME, forwarder, changeCheckerFactoryMock, currCtx);
+    indexEventHandler = new IndexEventHandler(forwarder, changeCheckerFactoryMock, currCtx);
   }
 
   @Test
   public void shouldIndexInRemoteOnChangeIndexedEvent() throws Exception {
     indexEventHandler.onChangeIndexed(PROJECT_NAME, changeId.get());
-    executorThread.join();
     verify(forwarder).indexChange(eq(PROJECT_NAME), eq(CHANGE_ID), any());
   }
 
@@ -144,21 +119,18 @@ public class IndexEventHandlerTest {
     setUpIndexEventHandler(new CurrentRequestContext(threadLocalCtxMock, cfgMock, oneOffCtxMock));
 
     indexEventHandler.onChangeIndexed(PROJECT_NAME, changeId.get());
-    executorThread.join();
     verify(forwarder).indexChange(eq(PROJECT_NAME), eq(CHANGE_ID), any());
   }
 
   @Test
   public void shouldIndexInRemoteOnAccountIndexedEvent() throws Exception {
     indexEventHandler.onAccountIndexed(accountId.get());
-    executorThread.join();
     verify(forwarder).indexAccount(eq(ACCOUNT_ID), any());
   }
 
   @Test
   public void shouldDeleteFromIndexInRemoteOnChangeDeletedEvent() throws Exception {
     indexEventHandler.onChangeDeleted(changeId.get());
-    executorThread.join();
     verify(forwarder).deleteChangeFromIndex(eq(CHANGE_ID), any());
     verifyNoInteractions(changeCheckerMock); // Deleted changes should not be checked against NoteDb
   }
@@ -166,7 +138,6 @@ public class IndexEventHandlerTest {
   @Test
   public void shouldIndexInRemoteOnGroupIndexedEvent() throws Exception {
     indexEventHandler.onGroupIndexed(accountGroupUUID.get());
-    executorThread.join();
     verify(forwarder).indexGroup(eq(UUID), any());
   }
 
@@ -195,248 +166,5 @@ public class IndexEventHandlerTest {
     indexEventHandler.onGroupIndexed(accountGroupUUID.get());
     Context.unsetForwardedEvent();
     verifyNoInteractions(forwarder);
-  }
-
-  @Test
-  public void duplicateChangeEventOfAQueuedEventShouldGetDiscarded() {
-    ScheduledThreadPoolExecutor poolMock = mock(ScheduledThreadPoolExecutor.class);
-    ScheduledThreadPoolExecutor poolBatchMock = mock(ScheduledThreadPoolExecutor.class);
-    indexEventHandler =
-        new IndexEventHandler(
-            poolMock, poolBatchMock, PLUGIN_NAME, forwarder, changeCheckerFactoryMock, currCtx);
-    indexEventHandler.onChangeIndexed(PROJECT_NAME, changeId.get());
-    indexEventHandler.onChangeIndexed(PROJECT_NAME, changeId.get());
-    verify(poolMock, times(1))
-        .execute(indexEventHandler.new IndexChangeTask(PROJECT_NAME, CHANGE_ID, null));
-  }
-
-  @Test
-  public void duplicateAccountEventOfAQueuedEventShouldGetDiscarded() {
-    ScheduledThreadPoolExecutor poolMock = mock(ScheduledThreadPoolExecutor.class);
-    ScheduledThreadPoolExecutor poolBatchMock = mock(ScheduledThreadPoolExecutor.class);
-    indexEventHandler =
-        new IndexEventHandler(
-            poolMock, poolBatchMock, PLUGIN_NAME, forwarder, changeCheckerFactoryMock, currCtx);
-    indexEventHandler.onAccountIndexed(accountId.get());
-    indexEventHandler.onAccountIndexed(accountId.get());
-    verify(poolMock, times(1)).execute(indexEventHandler.new IndexAccountTask(ACCOUNT_ID));
-  }
-
-  @Test
-  public void duplicateGroupEventOfAQueuedEventShouldGetDiscarded() {
-    ScheduledThreadPoolExecutor poolMock = mock(ScheduledThreadPoolExecutor.class);
-    ScheduledThreadPoolExecutor poolBatchMock = mock(ScheduledThreadPoolExecutor.class);
-    indexEventHandler =
-        new IndexEventHandler(
-            poolMock, poolBatchMock, PLUGIN_NAME, forwarder, changeCheckerFactoryMock, currCtx);
-    indexEventHandler.onGroupIndexed(accountGroupUUID.get());
-    indexEventHandler.onGroupIndexed(accountGroupUUID.get());
-    verify(poolMock, times(1)).execute(indexEventHandler.new IndexGroupTask(UUID));
-  }
-
-  @Test
-  public void testIndexChangeTaskToString() throws Exception {
-    IndexChangeTask task = indexEventHandler.new IndexChangeTask(PROJECT_NAME, CHANGE_ID, null);
-    assertThat(task.toString())
-        .isEqualTo(
-            String.format("[%s] Index change %s in target instance", PLUGIN_NAME, CHANGE_ID));
-  }
-
-  @Test
-  public void testIndexAccountTaskToString() throws Exception {
-    IndexAccountTask task = indexEventHandler.new IndexAccountTask(ACCOUNT_ID);
-    assertThat(task.toString())
-        .isEqualTo(
-            String.format("[%s] Index account %s in target instance", PLUGIN_NAME, ACCOUNT_ID));
-  }
-
-  @Test
-  public void testIndexGroupTaskToString() throws Exception {
-    IndexGroupTask task = indexEventHandler.new IndexGroupTask(UUID);
-    assertThat(task.toString())
-        .isEqualTo(String.format("[%s] Index group %s in target instance", PLUGIN_NAME, UUID));
-  }
-
-  @Test
-  public void testIndexChangeTaskHashCodeAndEquals() {
-    IndexChangeTask task = indexEventHandler.new IndexChangeTask(PROJECT_NAME, CHANGE_ID, null);
-
-    IndexChangeTask sameTask = task;
-    assertThat(task.equals(sameTask)).isTrue();
-    assertThat(task.hashCode()).isEqualTo(sameTask.hashCode());
-
-    IndexChangeTask identicalTask =
-        indexEventHandler.new IndexChangeTask(PROJECT_NAME, CHANGE_ID, null);
-    assertThat(task.equals(identicalTask)).isTrue();
-    assertThat(task.hashCode()).isEqualTo(identicalTask.hashCode());
-
-    assertThat(task.equals(null)).isFalse();
-    assertThat(
-            task.equals(indexEventHandler.new IndexChangeTask(PROJECT_NAME, CHANGE_ID + 1, null)))
-        .isFalse();
-    assertThat(task.hashCode()).isNotEqualTo("test".hashCode());
-
-    IndexChangeTask differentChangeIdTask =
-        indexEventHandler.new IndexChangeTask(PROJECT_NAME, 123, null);
-    assertThat(task.equals(differentChangeIdTask)).isFalse();
-    assertThat(task.hashCode()).isNotEqualTo(differentChangeIdTask.hashCode());
-  }
-
-  @Test
-  public void testDeleteChangeTaskHashCodeAndEquals() {
-    DeleteChangeTask task = indexEventHandler.new DeleteChangeTask(CHANGE_ID, null);
-
-    DeleteChangeTask sameTask = task;
-    assertThat(task.equals(sameTask)).isTrue();
-    assertThat(task.hashCode()).isEqualTo(sameTask.hashCode());
-
-    DeleteChangeTask identicalTask = indexEventHandler.new DeleteChangeTask(CHANGE_ID, null);
-    assertThat(task.equals(identicalTask)).isTrue();
-    assertThat(task.hashCode()).isEqualTo(identicalTask.hashCode());
-
-    assertThat(task.equals(null)).isFalse();
-    assertThat(task.equals(indexEventHandler.new DeleteChangeTask(CHANGE_ID + 1, null))).isFalse();
-    assertThat(task.hashCode()).isNotEqualTo("test".hashCode());
-
-    DeleteChangeTask differentChangeIdTask = indexEventHandler.new DeleteChangeTask(123, null);
-    assertThat(task.equals(differentChangeIdTask)).isFalse();
-    assertThat(task.hashCode()).isNotEqualTo(differentChangeIdTask.hashCode());
-  }
-
-  @Test
-  public void testIndexAccountTaskHashCodeAndEquals() {
-    IndexAccountTask task = indexEventHandler.new IndexAccountTask(ACCOUNT_ID);
-
-    IndexAccountTask sameTask = task;
-    assertThat(task.equals(sameTask)).isTrue();
-    assertThat(task.hashCode()).isEqualTo(sameTask.hashCode());
-
-    IndexAccountTask identicalTask = indexEventHandler.new IndexAccountTask(ACCOUNT_ID);
-    assertThat(task.equals(identicalTask)).isTrue();
-    assertThat(task.hashCode()).isEqualTo(identicalTask.hashCode());
-
-    assertThat(task.equals(null)).isFalse();
-    assertThat(task.equals(indexEventHandler.new IndexAccountTask(ACCOUNT_ID + 1))).isFalse();
-    assertThat(task.hashCode()).isNotEqualTo("test".hashCode());
-
-    IndexAccountTask differentAccountIdTask = indexEventHandler.new IndexAccountTask(123);
-    assertThat(task.equals(differentAccountIdTask)).isFalse();
-    assertThat(task.hashCode()).isNotEqualTo(differentAccountIdTask.hashCode());
-  }
-
-  @Test
-  public void testIndexGroupTaskHashCodeAndEquals() {
-    IndexGroupTask task = indexEventHandler.new IndexGroupTask(UUID);
-
-    IndexGroupTask sameTask = task;
-    assertThat(task.equals(sameTask)).isTrue();
-    assertThat(task.hashCode()).isEqualTo(sameTask.hashCode());
-
-    IndexGroupTask identicalTask = indexEventHandler.new IndexGroupTask(UUID);
-    assertThat(task.equals(identicalTask)).isTrue();
-    assertThat(task.hashCode()).isEqualTo(identicalTask.hashCode());
-
-    assertThat(task.equals(null)).isFalse();
-    assertThat(task.equals(indexEventHandler.new IndexGroupTask(OTHER_UUID))).isFalse();
-    assertThat(task.hashCode()).isNotEqualTo("test".hashCode());
-
-    IndexGroupTask differentGroupIdTask = indexEventHandler.new IndexGroupTask("123");
-    assertThat(task.equals(differentGroupIdTask)).isFalse();
-    assertThat(task.hashCode()).isNotEqualTo(differentGroupIdTask.hashCode());
-  }
-
-  private class CurrentThreadScheduledExecutorService implements ScheduledExecutorService {
-    @Override
-    public void shutdown() {}
-
-    @Override
-    public List<Runnable> shutdownNow() {
-      return null;
-    }
-
-    @Override
-    public boolean isShutdown() {
-      return false;
-    }
-
-    @Override
-    public boolean isTerminated() {
-      return false;
-    }
-
-    @Override
-    public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
-      return false;
-    }
-
-    @Override
-    public <T> Future<T> submit(Callable<T> task) {
-      return null;
-    }
-
-    @Override
-    public <T> Future<T> submit(Runnable task, T result) {
-      return null;
-    }
-
-    @Override
-    public Future<?> submit(Runnable task) {
-      return null;
-    }
-
-    @Override
-    public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks)
-        throws InterruptedException {
-      return null;
-    }
-
-    @Override
-    public <T> List<Future<T>> invokeAll(
-        Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit)
-        throws InterruptedException {
-      return null;
-    }
-
-    @Override
-    public <T> T invokeAny(Collection<? extends Callable<T>> tasks)
-        throws InterruptedException, ExecutionException {
-      return null;
-    }
-
-    @Override
-    public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit)
-        throws InterruptedException, ExecutionException, TimeoutException {
-      return null;
-    }
-
-    @Override
-    public void execute(Runnable command) {
-      executorThread = new Thread(command);
-      executorThread.setName(EXECUTOR_THREAD_NAME);
-      executorThread.start();
-    }
-
-    @Override
-    public ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit) {
-      command.run();
-      return null;
-    }
-
-    @Override
-    public <V> ScheduledFuture<V> schedule(Callable<V> callable, long delay, TimeUnit unit) {
-      return null;
-    }
-
-    @Override
-    public ScheduledFuture<?> scheduleAtFixedRate(
-        Runnable command, long initialDelay, long period, TimeUnit unit) {
-      return null;
-    }
-
-    @Override
-    public ScheduledFuture<?> scheduleWithFixedDelay(
-        Runnable command, long initialDelay, long delay, TimeUnit unit) {
-      return null;
-    }
   }
 }
