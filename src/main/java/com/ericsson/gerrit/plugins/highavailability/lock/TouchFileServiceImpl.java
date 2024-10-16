a6db9f5af18930dec67cd76b7cbc02d1cf7e634b
Saša Živkov <sasa.zivkov@sap.com>
2024-10-16 16:28:14 +0200

    Implement LockManager extension using files to represent locks
    The LockManager extension allows plugins to provide a custom
    implementation. The high-availability plugin needs to ensure that locks
    created by one primary server are honored by the other primary server.
    
    Use $SHARED_DIR/locks directory to persist locks. Each lock is
    represented by one file. The file name is SHA-1 of the lock name. The
    lock name is persisted as the content of the file for easier support.
    Once created, a lock is kept alive by a background job touching it every
    second. This keeps changing its last-updated timestamp. When lock is
    regularly released the file representing that lock is deleted. In
    addition, every created lock file is registered to be deleted when the
    JVM terminates. In case where JVM running Gerrit crashes or gets
    forcefully terminated (kill -9) the lock files may not get deleted and
    they become stale. Another background job scans all lock files
    periodically and deletes all those representing stale locks.
    
    Change-Id: I6eb4e1f5f7005691279781690df516d5f95668d5

---
+++ src/main/java/com/ericsson/gerrit/plugins/highavailability/lock/TouchFileServiceImpl.java
@@ -0,0 +1,53 @@
+// Copyright (C) 2024 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.ericsson.gerrit.plugins.highavailability.lock;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.io.File;
+import java.time.Duration;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+@Singleton
+public class TouchFileServiceImpl implements TouchFileService {
+
+  private final ScheduledExecutorService executor;
+  private final Duration interval;
+
+  @Inject
+  public TouchFileServiceImpl(
+      @TouchFileExecutor ScheduledExecutorService executor, @TouchFileInterval Duration interval) {
+    this.executor = executor;
+    this.interval = interval;
+  }
+
+  @Override
+  public ScheduledFuture<?> touchForever(File file) {
+    return executor.scheduleAtFixedRate(
+        () -> touch(file), interval.getSeconds(), interval.getSeconds(), TimeUnit.SECONDS);
+  }
+
+  private static void touch(File f) {
+    boolean succeeded = f.setLastModified(System.currentTimeMillis());
+    if (!succeeded) {
+      if (!f.exists()) {
+        throw new RuntimeException(
+            String.format("File %s doesn't exist, stopping the touch task", f.toPath()));
+      }
+    }
+  }
+}
