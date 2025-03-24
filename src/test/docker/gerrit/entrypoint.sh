cd6c0b57dc83b2b587d338e1c9c12074be0bbadd
Luca Milanesio <luca.milanesio@gmail.com>
2025-03-24 13:44:40 +0000

    Use a proper NFSv4 client for mounting the shared git
    In order to properly test a real-life NFS setup, prefer
    to use a proper NFSv4 client rather than using the Docker
    volume driver which would not implement the client caching
    layer that is typically needed in production to optimise
    read performance.
    
    Also define a 2 minutes attribute caching and the
    associated jgit.config that production setups would need
    with Gerrit in HA.
    
    Change-Id: I2ae5f8d4605250dfbcedb625a286f3022dc1fec7

--- src/test/docker/gerrit/entrypoint.sh
+++ src/test/docker/gerrit/entrypoint.sh
@@ -6,9 +6,14 @@ then
 fi
 
 chown -R gerrit /var/gerrit/etc
+sudo -u gerrit cp /var/gerrit/etc/jgit.config.orig /var/gerrit/etc/jgit.config
 sudo -u gerrit cp /var/gerrit/etc/gerrit.config.orig /var/gerrit/etc/gerrit.config
 sudo -u gerrit cp /var/gerrit/etc/high-availability.config.orig /var/gerrit/etc/high-availability.config
 
+
+echo "Mount NFS ..."
+mkdir /var/gerrit/git && chown gerrit:gerrit /var/gerrit/git
+mount -t nfs4 -o actimeo=120 nfs-server:/git /var/gerrit/git
 
 echo "Init gerrit..."
 sudo -u gerrit java -jar /tmp/gerrit.war init -d /var/gerrit --batch --install-all-plugins
