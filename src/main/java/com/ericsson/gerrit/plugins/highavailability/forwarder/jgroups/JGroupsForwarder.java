82e6cf398de5f7a14a457f66523ab265b864f9a0
Thomas Dräbing <thomas.draebing@sap.com>
2025-10-01 15:13:03 +0200

    Use Java record for Forwarder.Result
    Change-Id: I475d55b33aff1c5b6e893178779fd0eda269cac1

--- src/main/java/com/ericsson/gerrit/plugins/highavailability/forwarder/jgroups/JGroupsForwarder.java
+++ src/main/java/com/ericsson/gerrit/plugins/highavailability/forwarder/jgroups/JGroupsForwarder.java
@@ -63,7 +63,7 @@ public class JGroupsForwarder implements Forwarder {
     this.metricsRegistry = metricsRegistry;
     this.executor.onComplete(
         ev -> {
-          this.metricsRegistry.get(ev.getResult().getType()).recordRetries(ev.getAttemptCount());
+          this.metricsRegistry.get(ev.getResult().type()).recordRetries(ev.getAttemptCount());
         });
   }
 
@@ -129,7 +129,7 @@ public class JGroupsForwarder implements Forwarder {
         .getAsync(() -> executeOnce(cmd))
         .thenApplyAsync(
             result -> {
-              metricsRegistry.get(cmd.type).recordResult(result.getResult());
+              metricsRegistry.get(cmd.type).recordResult(result.result());
               metricsRegistry
                   .get(cmd.type)
                   .recordLatency(Instant.now().toEpochMilli() - cmd.eventCreatedOn);
