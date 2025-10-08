2e3c1cafe5db91ba6e1fa149da2240a8ee9cf9e6
Saša Živkov <sasa.zivkov@sap.com>
2025-10-08 13:00:37 +0200

    Add support for PubSub for event forwarding
    The high-availability plugin currently supports two methods for
    peer discovery (static and jgroups) and transport (http and jgroups).
    These methods do not rely on third party services but are hard to
    configure and prone to loosing events, e.g. during restarts. A PubSub
    system would be much better suited in that regard, because
    
    - no peer discovery is required, since all Gerrit instances can
      just publish and subscribe to the same message queue (topic)
    - messages can be retained and can be delivered at a later point
      in time, e.g. after a Gerrit instance has finished restarting.
    
    This change adds support for the GCP PubSub service.
    
    Change-Id: Ifd67cc6eeb29027c55d5e9723eb5e9f31abe26a5

---
+++ src/main/java/com/ericsson/gerrit/plugins/highavailability/forwarder/pubsub/OnStartStop.java
@@ -0,0 +1,72 @@
+// Copyright (C) 2024 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.ericsson.gerrit.plugins.highavailability.forwarder.pubsub;
+
+import com.ericsson.gerrit.plugins.highavailability.Configuration;
+import com.google.cloud.pubsub.v1.Publisher;
+import com.google.cloud.pubsub.v1.Subscriber;
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.extensions.events.LifecycleListener;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+@Singleton
+public class OnStartStop implements LifecycleListener {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private final Configuration config;
+  private final Publisher publisher;
+  private final Subscriber subscriber;
+
+  @Inject
+  public OnStartStop(Configuration config, Publisher publisher, Subscriber subscriber) {
+    this.config = config;
+    this.publisher = publisher;
+    this.subscriber = subscriber;
+  }
+
+  @Override
+  public void start() {
+    try {
+      subscriber
+          .startAsync()
+          .awaitRunning(config.pubSub().subscriptionTimeout().getSeconds(), TimeUnit.SECONDS);
+    } catch (TimeoutException e) {
+      throw new IllegalStateException("Timeout while subscribing to PubSub topic", e);
+    }
+  }
+
+  @Override
+  public void stop() {
+    logger.atInfo().log("Closing PubSub publisher");
+    try {
+      publisher.shutdown();
+      publisher.awaitTermination(config.pubSub().shutdownTimeout().getSeconds(), TimeUnit.SECONDS);
+    } catch (InterruptedException e) {
+      logger.atSevere().withCause(e).log("Could not close the MessageDispatcher");
+    }
+
+    logger.atInfo().log("Closing PubSub subscriber");
+    try {
+      subscriber
+          .stopAsync()
+          .awaitTerminated(config.pubSub().shutdownTimeout().getSeconds(), TimeUnit.SECONDS);
+    } catch (TimeoutException e) {
+      logger.atSevere().withCause(e).log("Timeout during stopping of subscription.");
+    }
+  }
+}
