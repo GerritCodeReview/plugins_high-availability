ef6be2723b39be5ddad631e857e56b412563ab5a
Saša Živkov <sasa.zivkov@sap.com>
2026-03-06 12:19:02 +0100

    Define ApiModule: expose Forwarder, CommandProcessor and Commands
    This change exposes DynamicItem<Forwarder> so that other plugins can
    implement message forwarding using technology of their choice, for
    example using a GCP pubsub.
    
    Also expose DynamicItem<CommandProcessor>. This is not exposed to be
    overridden but only as a mean for other plugins to inject it and pass
    received commands back to the high-availability plugin.
    
    Change-Id: I57c8b27fa4caf6109d797494fac1a91789f056a0

--- src/main/java/com/ericsson/gerrit/plugins/highavailability/forwarder/jgroups/JGroupsForwarderModule.java
+++ src/main/java/com/ericsson/gerrit/plugins/highavailability/forwarder/jgroups/JGroupsForwarderModule.java
@@ -18,6 +18,7 @@ import com.ericsson.gerrit.plugins.highavailability.forwarder.Forwarder;
 import com.ericsson.gerrit.plugins.highavailability.forwarder.Forwarder.Result;
 import com.ericsson.gerrit.plugins.highavailability.forwarder.InstantTypeAdapter;
 import com.ericsson.gerrit.plugins.highavailability.peers.jgroups.JChannelProviderModule;
+import com.google.gerrit.extensions.registration.DynamicItem;
 import com.google.gerrit.lifecycle.LifecycleModule;
 import com.google.inject.Scopes;
 import com.google.inject.TypeLiteral;
@@ -29,7 +30,7 @@ public class JGroupsForwarderModule extends LifecycleModule {
 
   @Override
   protected void configure() {
-    bind(Forwarder.class).to(JGroupsForwarder.class);
+    DynamicItem.bind(binder(), Forwarder.class).to(JGroupsForwarder.class);
     bind(MessageDispatcher.class).toProvider(MessageDispatcherProvider.class).in(Scopes.SINGLETON);
     bind(RequestHandler.class).to(JGroupsMessageProcessor.class);
     install(new JChannelProviderModule());
